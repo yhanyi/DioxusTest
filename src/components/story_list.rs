10d24de402fc29ae6d4416c26916b71575cd26c5
yhanyi <yeohhanyi0916@gmail.com>
2024-08-09 22:40:31 +0800

    Updated storylist component

--- src/components/story_list.rs
+++ src/components/story_list.rs
@@ -46,12 +46,35 @@ pub struct StoryPageData {
 #[component]
 pub fn StoryListing(story: ReadOnlySignal<StoryItem>) -> Element {
     let StoryItem { title, url, by, score, time, children, .. } = &*story.read();
-
-    let comments = children.len();
-
+    let url = url.as_deref().unwrap_or_default();
+    let hostname = url
+        .trim_start_matches("https://")
+        .trim_start_matches("http://")
+        .trim_start_matches("www.");
+    let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
+    let comments = format!("{} {}", children.len(), if children.len() == 1 {
+        " comment"
+    } else {
+        " comments"
+    });
+    let time = time.format("%D %l:%M %p");
     rsx! {
-        div {padding: "0.5rem", position:"relative",
-              h1{"{title} by {by} ({score}) {time} {comments}"}
+        div { padding: "0.5rem", position: "relative", font_family: "Arial, sans-serif",
+            div { font_size: "1.5rem",
+                a { href: url, "{title}" }
+                a {
+                    color: "gray",
+                    href: "https://news.ycombinator.com/from?site={hostname}",
+                    text_decoration: "none",
+                    " ({hostname})"
+                }
             }
+            div { display: "flex", flex_direction: "row", color: "gray",
+                div { "{score}" }
+                div { padding_left: "0.5rem", "by {by}" }
+                div { padding_left: "0.5rem", "{time}" }
+                div { padding_left: "0.5rem", "{comments}" }
+            }
+        }
     }
 }
