b5398d9ae96700b3d1d2ab3956e280fc759edd3b
yhanyi <yeohhanyi0916@gmail.com>
2024-08-10 00:54:40 +0800

    Updated data fetching for stories

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,3 +2,4 @@ pub mod high_five;
 pub mod story_list;
 pub mod preview;
 pub mod stories;
+pub mod api;
--- src/components/stories.rs
+++ src/components/stories.rs
@@ -1,22 +1,46 @@
 use dioxus::prelude::*;
-use crate::components::story_list::{ StoryListing, StoryItem };
+use crate::components::story_list::StoryListing;
+use crate::components::api::get_stories;
 
 #[component]
 pub fn Stories() -> Element {
-    rsx! {
-        StoryListing {
-            story: StoryItem {
-                id: 0,
-                title: "Hello".to_string(),
-                url: None,
-                text: None,
-                by: "Me".to_string(),
-                score: 0,
-                descendants: 0,
-                time: chrono::Utc::now(),
-                children: vec![],
-                r#type: "".to_string(),
-            },
+    let stories = use_resource(move || get_stories(10));
+    match &*stories.read_unchecked() {
+        Some(Ok(list)) => {
+            rsx! {
+              div {
+                for story in list {
+                  StoryListing {story: story.clone()}
+                }
+              }
+            }
+        }
+        Some(Err(err)) => {
+            rsx! {
+                "An error occurred while fetching stories {err}"
+            }
+        }
+        None => {
+            rsx! {
+                "Loading..."
+            }
         }
     }
+
+    // rsx! {
+    //     StoryListing {
+    //         story: StoryItem {
+    //             id: 0,
+    //             title: "Hello".to_string(),
+    //             url: None,
+    //             text: None,
+    //             by: "Me".to_string(),
+    //             score: 0,
+    //             descendants: 0,
+    //             time: chrono::Utc::now(),
+    //             children: vec![],
+    //             r#type: "".to_string(),
+    //         },
+    //     }
+    // }
 }
